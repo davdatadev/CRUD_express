d7787f27e1a5fe7883a7e7e015e8fc60d2490bb0
David Arenas <datadavdev@gmail.com>
2025-11-17 21:24:04 -0500

    Implementar Handlebars y reestructurar vistas; agregar encabezado y página de inicio

--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -1,11 +1,3 @@
-// import { fileURLToPath } from "url"
-// import { dirname, join } from "path"
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
-
-export const rootHandler = (req, res) => { 
-    // const filePath = join(__dirname, "..", "views", "index.html")
-    // res.sendFile(filePath)
-    res.render("index")
+export const rootHandler = (req, res) => {
+    res.render("home")
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import express from "express"
-import { PORT } from "./const/constantes.js"
+import handlebars from "express-handlebars"
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 import rootRouter from "./routes/root.routes.js"
 import productsRouter from "./routes/products.routes.js"
 import cartsRouter from "./routes/carts.routes.js"
-import handlebars from "express-handlebars"
-
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path' 
+import { PORT } from "./const/constantes.js"
 
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const __publicdir = join(__dirname, 'public')
 
 // Crear servidor
 const app = express()
 
-// Configurar Handlebars
-app.engine("handlebars", handlebars.engine())
-app.set("view engine", "handlebars")
-app.set("views", join(__dirname, "views"))
+// Configuración más personalizable de Handlebars
+const hbs = handlebars.create({
+    extname: '.handlebars', // La extensión de tus archivos de plantilla
+    layoutsDir: join(__dirname, 'views', 'layouts'), // Ruta de layouts
+    partialsDir: join(__dirname, 'views', 'partials'), // Ruta de partials
+    defaultLayout: 'main',
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', join(__dirname, 'views'));
 
 // Middlewares
 app.use(express.json())
+app.use(express.static(__publicdir))
 
 app.use("/", rootRouter)
 app.use("/products", productsRouter)
